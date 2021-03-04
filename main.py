363a2ab38ebe91242a12fe1056a8ccf95fe2aa93
Ashwini Koshta <71019487+Ashwini-1401@users.noreply.github.com>
2021-03-04 09:11:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+
+# print("Hi I am Ashwini")
+#
+# x = "Ashwini"
+#
+# print(f"I am {x}")
+# if x in "Ashwinikoshta":
+#     print("yes")
+#
+# class Parent:
+#     def __init__(self , fname , lname):
+#         self.fname = fname
+#         self.lname = lname
+#     def function(self):
+#         print(self.fname + self.lname)
+#
+# class Child(Parent):
+#     def __init__(self , fname , lname):
+#         self.fname = fname
+#         self.lname = lname
+#
+# parentObj = Parent("Ashwini" , "Koshta")
+# parentObj.function()
+#
+# childObj = Child("Sam", "Joe")
+# print( childObj.lname)
+
+# file = open("text.txt", "w")
+# file.write("\n I am third line")
+# file.write("i am fourth hee")
+
+
+os.remove("text.txt")
